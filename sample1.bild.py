cf7bc973e3021de3ae69dfe739eb889d65ef0291
Terence Parr <parrt@antlr.org>
2014-03-05 11:44:27 -0800

    re-add bild.py; not sure why it didn't make it to github from work machine

---
+++ sample1.bild.py
@@ -0,0 +1,32 @@
+from bild import *
+
+def T(): # there is a T.g4 grammar in src/grammars
+	antlr("src/grammars", "gen/org/foo", package="org.foo")
+
+def compile():
+	require(T())
+	javac("src/java", "out")
+	javac("gen", "out")
+
+def mkjar():
+	require(compile())
+	jar("dist", "app.jar", ["out","resources"])
+
+def all():
+	mkjar()
+
+def clean():
+	rmdir("out")
+	rmdir("gen")
+
+def clean_all():
+	require(clean())
+	rmdir("dist")
+
+"""
+manifest {
+        attributes("Implementation-Title": "Gradle", "Implementation-Version": version)
+    }
+"""
+
+build(globals()) # if you want cmd-line arg processing. Or, just call your target

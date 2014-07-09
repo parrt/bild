6aff4a4fd12195b710bcfb2725b742fc4f6521cc
Terence Parr <parrt@cs.usfca.edu>
2014-07-08 17:17:08 -0700

    make sample bootstrap

--- examples/proj1/bild.py
+++ examples/proj1/bild.py
@@ -1,35 +1,50 @@
-from bilder import *
+#!/usr/bin/env python
+# bootstrap by downloading bilder.py if not found
+import urllib
 
-# can set as you like:
-# CLASSPATH = ...
+if not os.path.exists("bilder.py"):
+    print "bootstrapping; downloading bilder.py"
+    urllib.urlretrieve(
+        "https://raw.githubusercontent.com/parrt/bild/master/src/python/bilder.py",
+        "bilder.py")
+
+# assumes bilder.py is in current directory
+from bilder import *
 
 projversion = 1.0
 
-def T(): # there is a T.g4 grammar in src/grammars
-	antlr4("src/grammars", "gen/org/foo", package="org.foo")
+
+def parser():  # there is a T.g4 grammar in src/grammars
+    antlr4("src/grammars", "gen", package="org.foo")
+
 
 def compile():
-	require(T)
-	javac("src/java", "out")
-	javac("gen", "out")
+    require(parser)
+    javac("src/java", "out")
+    javac("gen", "out")
+
 
 def mkjar():
-	require(compile)
-	copytree(src="resources", dst="out/resources")
-	manifest = """Version: %s
+    require(compile)
+    copytree(src="resources", dst="out/resources")
+    manifest = """Version: %s
 Main-Class: org.foo.Blort
 """ % projversion
-	jar("dist/app.jar", srcdir="out", manifest=manifest)
+    jar("dist/app.jar", srcdir="out", manifest=manifest)
+
 
 def all():
-	mkjar()
+    mkjar()
+
 
 def clean():
-	rmdir("out")
-	rmdir("gen")
+    rmdir("out")
+    rmdir("gen")
+
 
 def clean_all():
-	require(clean)
-	rmdir("dist")
+    require(clean)
+    rmdir("dist")
 
-processargs(globals()) # if you want cmd-line arg processing. Or, just call your target
+
+processargs(globals())  # if you want cmd-line arg processing. Or, just call your target
